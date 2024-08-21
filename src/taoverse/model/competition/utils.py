3331bdc2288480329191ee03850253d29470c1f7
Sid <sid.data.universe@gmail.com>
2024-08-21 10:19:45 -0700

    Add helper to calculate epsilon for the earlier model.

--- src/taoverse/model/competition/utils.py
+++ src/taoverse/model/competition/utils.py
@@ -1,6 +1,6 @@
 from typing import List, Optional, Tuple
 
-from taoverse.model.competition.data import Competition
+from taoverse.model.competition.data import Competition, EpsilonDecay
 
 
 def get_competition_for_block(
@@ -28,3 +28,22 @@ def get_competition_schedule_for_block(
         competition_schedule is not None
     ), f"No competition schedule found for block {block}"
     return competition_schedule
+
+
+def get_epsilon_for_earlier_model(
+    current_block: int, earlier_model_block: int, epsilon_decay: EpsilonDecay
+) -> float:
+    """Calculates epsilon value to use for the earlier model in a comparison based on epsilon decay."""
+
+    # Find the difference in blocks.
+    # In case of metagraph divergance between checking blocks and syncing models defult to 0.
+    block_difference = max(current_block - earlier_model_block, 0)
+
+    # Use a linear progression based on the decay blocks capping at complete decay.
+    block_adjustment = min(block_difference / epsilon_decay.decay_blocks, 1)
+
+    epsilon_adjustment = block_adjustment * (
+        epsilon_decay.starting_epsilon - epsilon_decay.ending_epsilon
+    )
+
+    return epsilon_decay.starting_epsilon - epsilon_adjustment
--- tests/model/competition/test_competition_utils.py
+++ tests/model/competition/test_competition_utils.py
@@ -12,10 +12,16 @@ from transformers import (
 )
 from typing import Dict, List, Tuple
 
-from taoverse.model.competition.data import Competition, ModelConstraints
+from taoverse.model.competition.data import (
+    Competition,
+    ModelConstraints,
+    NormValidationConstraints,
+    EpsilonDecay,
+)
 from taoverse.model.competition.utils import (
     get_competition_for_block,
     get_competition_schedule_for_block,
+    get_epsilon_for_earlier_model,
 )
 
 unittest.util._MAX_LENGTH = 2000
@@ -42,6 +48,14 @@ class TestCompetitionUtils(unittest.TestCase):
                 "torch_dtype": torch.bfloat16,
             },
             eval_block_delay=1200,  # ~4 hours.
+            norm_validation_constraints=NormValidationConstraints(
+                norm_eps_soft=200,
+                norm_eps_soft_percent_threshold=0.15,
+                norm_eps_hard=1000,
+            ),
+            epsilon_decay=EpsilonDecay(
+                starting_epsilon=0.005, ending_epsilon=0.001, decay_blocks=7200 * 7
+            ),
         ),
     }
 
@@ -78,6 +92,14 @@ class TestCompetitionUtils(unittest.TestCase):
                 kwargs={
                     "torch_dtype": torch.bfloat16,
                 },
+                norm_validation_constraints=NormValidationConstraints(
+                    norm_eps_soft=200,
+                    norm_eps_soft_percent_threshold=0.15,
+                    norm_eps_hard=1000,
+                ),
+                epsilon_decay=EpsilonDecay(
+                    starting_epsilon=0.005, ending_epsilon=0.001, decay_blocks=7200 * 7
+                ),
             ),
             reward_percentage=1.0,
         )
@@ -124,6 +146,16 @@ class TestCompetitionUtils(unittest.TestCase):
                     kwargs={
                         "torch_dtype": torch.bfloat16,
                     },
+                    norm_validation_constraints=NormValidationConstraints(
+                        norm_eps_soft=200,
+                        norm_eps_soft_percent_threshold=0.15,
+                        norm_eps_hard=1000,
+                    ),
+                    epsilon_decay=EpsilonDecay(
+                        starting_epsilon=0.005,
+                        ending_epsilon=0.001,
+                        decay_blocks=7200 * 7,
+                    ),
                 ),
                 reward_percentage=1.0,
             ),
@@ -139,6 +171,65 @@ class TestCompetitionUtils(unittest.TestCase):
             _ = get_competition_schedule_for_block(
                 block=-1, schedule_by_block=self.COMPETITION_SCHEDULE_BY_BLOCK
             )
+
+    def test_get_epsilon_for_earlier_model_earlier_block(self):
+        epsilon_decay = EpsilonDecay(
+            starting_epsilon=0.005,
+            ending_epsilon=0.001,
+            decay_blocks=7200 * 7,
+        )
+
+        calculated_epsilon = get_epsilon_for_earlier_model(
+            current_block=0, earlier_model_block=7200, epsilon_decay=epsilon_decay
+        )
+
+        self.assertEqual(calculated_epsilon, epsilon_decay.starting_epsilon)
+
+    def test_get_epsilon_for_earlier_model_same_block(self):
+        epsilon_decay = EpsilonDecay(
+            starting_epsilon=0.005,
+            ending_epsilon=0.001,
+            decay_blocks=7200 * 7,
+        )
+
+        calculated_epsilon = get_epsilon_for_earlier_model(
+            current_block=7200, earlier_model_block=7200, epsilon_decay=epsilon_decay
+        )
+
+        self.assertEqual(calculated_epsilon, epsilon_decay.starting_epsilon)
+
+    def test_get_epsilon_for_earlier_model_middle_block(self):
+        epsilon_decay = EpsilonDecay(
+            starting_epsilon=0.005,
+            ending_epsilon=0.001,
+            decay_blocks=7200 * 7,
+        )
+
+        calculated_epsilon = get_epsilon_for_earlier_model(
+            current_block=epsilon_decay.decay_blocks / 2,
+            earlier_model_block=0,
+            epsilon_decay=epsilon_decay,
+        )
+
+        self.assertEqual(
+            calculated_epsilon,
+            (epsilon_decay.starting_epsilon + epsilon_decay.ending_epsilon) / 2,
+        )
+
+    def test_get_epsilon_for_earlier_model_max_block(self):
+        epsilon_decay = EpsilonDecay(
+            starting_epsilon=0.005,
+            ending_epsilon=0.001,
+            decay_blocks=7200 * 7,
+        )
+
+        calculated_epsilon = get_epsilon_for_earlier_model(
+            current_block=7200 * 8,
+            earlier_model_block=7200,
+            epsilon_decay=epsilon_decay,
+        )
+
+        self.assertEqual(calculated_epsilon, epsilon_decay.ending_epsilon)
 
 
 if __name__ == "__main__":
